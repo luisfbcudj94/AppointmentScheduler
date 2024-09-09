84e9683f8e32a88c8230801deb67edd4189bf21d
LuisPerez <alejandroabril98@gmail.com>
2024-09-09 08:30:34 -0500

    feat: create service

---
+++ FrontEnd/appointment-scheduler/src/app/services/appointment.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+  private apiUrl = 'https://localhost:7070/api';
+
+  constructor(private http: HttpClient) { }
+
+  getAppointments(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/Appointment`);
+  }
+
+  getAppointment(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/Appointment/${id}`);
+  }
+
+  createAppointment(appointment: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, appointment);
+  }
+
+  updateAppointment(id: string, appointment: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/Appointment/${id}`, appointment);
+  }
+
+  deleteAppointment(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
